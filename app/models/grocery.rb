5f00ece6b8a6e0945a1b353405935d320eb3dce8
Susannah Sun <susannahsun@Susannahs-MacBook-Pro.local>
2021-01-15 18:10:21 -0800

    add case_sensitive:false to ingredient/leftover/grocery name and category

--- app/models/grocery.rb
+++ app/models/grocery.rb
@@ -3,8 +3,9 @@ class Grocery < ApplicationRecord
 
     belongs_to :user
 
-    validates :name, presence: true, uniqueness: {scope: :user}
-    validates_inclusion_of :category, :in => ['produce', 'meat', 'dairy', 'frozen', 'other'], :allow_nil => true
+    validates :name, presence: true, uniqueness: {scope: :user, case_sensitive: false}
+    validate :accepted_category
+    # validates_inclusion_of :category, :in => ['produce', 'meat', 'dairy', 'frozen', 'other'], :allow_nil => true
     validate :quantity_present
 
     private
@@ -12,5 +13,9 @@ class Grocery < ApplicationRecord
         if self.user_added == false
             self.errors.add(:quantity, 'quantity should be present') if self.quantity.to_s == ''
         end
+    end
+
+    def accepted_category
+        self.errors.add(:category, 'allowed categories: produce, meat, dairy, frozen, nuts & seeds, other') unless ['produce', 'meat', 'dairy', 'frozen', 'nuts & seeds', 'other'].include? self.category.downcase
     end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -6,9 +6,10 @@ class Ingredient < ApplicationRecord
   has_many :unit, through: :myrecipeingredientlinks, source: :myrecipe
   has_many :leftover_usages, dependent: :nullify
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, case_sensitive: false
   validate :singular
-  validates_inclusion_of :category, :in => ['produce', 'meat', 'dairy', 'frozen', 'nuts & seeds', 'other'], :allow_nil => false
+  validate :accepted_category
+  # validates_inclusion_of :category, :in => ['produce', 'meat', 'dairy', 'frozen', 'nuts & seeds', 'other'], :allow_nil => false
 
   before_validation :downcase_name
 
@@ -24,5 +25,9 @@ class Ingredient < ApplicationRecord
       if Ingredient.find_by(name: name.delete_suffix('es')) || Ingredient.find_by(name: name.delete_suffix('s'))
         self.errors.add(:name, 'please use singular form of ingredient name.')
       end
+    end
+
+    def accepted_category
+      self.errors.add(:category, 'allowed categories: produce, meat, dairy, frozen, nuts & seeds, other') unless ['produce', 'meat', 'dairy', 'frozen', 'nuts & seeds', 'other'].include? self.category.downcase
     end
 end
