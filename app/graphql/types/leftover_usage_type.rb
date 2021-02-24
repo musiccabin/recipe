b225e534d6c5d363cc321a93c2bd14d6666a210a
Susannah Sun <susannahsun@Susannahs-MacBook-Pro.local>
2021-02-23 22:35:50 -0800

    allow leftover_usage_type to have null unit

--- app/graphql/types/leftover_usage_type.rb
+++ app/graphql/types/leftover_usage_type.rb
@@ -2,7 +2,7 @@ module Types
   class LeftoverUsageType < Types::BaseObject
     field :id, ID, null: false
     field :quantity, String, null: false
-    field :unit, String, null: false
+    field :unit, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :user, Types::UserType, null: false
